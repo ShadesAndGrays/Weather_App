8116614cbbf3d8b65f52170343bba8582b463219
ShadesAndGrays <burner00shadow@gmail.com>
2024-02-29 18:46:44 +0100

    Added basic API call to OpenWeather

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(dead_code)]
+
+
+use dotenv::dotenv;
+use reqwest;
+use serde::Deserialize;
+use tokio;
+
+#[derive(Deserialize)]
+struct  TempData{
+    temp:f64
+}
+
+#[derive(Deserialize)]
+struct WeatherData{
+    main:TempData,
+    weather:Vec<WeatherDescription>
+}
+
+#[derive(Deserialize)]
+struct WeatherDescription{
+    description:String
+}
+
+#[tokio::main]
+async fn main(){
+    dotenv().ok();
+    let open_weather_api_key = std::env::var("API_KEY").expect("API_KEY must be set.");
+
+    // User input
+    let mut city = String::new();
+
+    std::io::stdin().read_line(&mut city).expect("Failed to read line");
+
+    tokio::spawn(fetch_weather(open_weather_api_key, city.clone()));
+
+    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+
+}
+
+async fn fetch_weather(api_key:String,city:String) -> Result<(),reqwest::Error>{
+    // First initialize url with city location and api key
+    let url = format!("https://api.openweathermap.org/data/2.5/weather?q={}&APPID={}",city.trim(),api_key);
+    println!("Url {}",url);
+    // Sendind request to openweather
+    let response = reqwest::get(&url).await?;
+    if response.status().is_success(){
+        let weather_data:WeatherData = response.json().await?;
+        let temperature = weather_data.main.temp;
+        let description = &weather_data.weather[0].description;
+        println!("Weather in {}: {:.2}°C, {}", city.trim(), temperature, description);       
+    }
+    else{
+        println!("Error: {}",response.status());
+    }
+    Ok(())
 }
