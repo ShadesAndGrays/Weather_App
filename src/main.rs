4986349f62ecc0d8d1dd02437e114eada7170fd9
ShadesAndGrays <burner00shadow@gmail.com>
2024-02-29 20:29:27 +0100

    Changed API call units to metric removed conversion function

--- src/main.rs
+++ src/main.rs
@@ -33,25 +33,22 @@ async fn main(){
 
     std::io::stdin().read_line(&mut city).expect("Failed to read line");
 
-    let _ = tokio::spawn(fetch_weather(open_weather_api_key, city.clone())).await.expect("Could not fetch fetch Weather");
+    let _ = tokio::spawn(fetch_weather(open_weather_api_key, city.clone())).await.expect("Could not fetch Weather");
 
     tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
 
 }
 
-fn ktoc(kelvin:f64) -> f64{
-    kelvin - 273.15
-}
 async fn fetch_weather(api_key:String,city:String) -> Result<(),reqwest::Error>{
     // First initialize url with city location and api key
-    let url = format!("https://api.openweathermap.org/data/2.5/weather?q={}&APPID={}",city.trim(),api_key);
+    let url = format!("https://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units=metric",city.trim(),api_key);
     // Sendind request to openweather
     let response = reqwest::get(&url).await?;
     if response.status().is_success(){
         let weather_data:WeatherData = response.json().await?;
         let temperature = weather_data.main.temp;
         let description = &weather_data.weather[0].description;
-        println!("Weather in {}: {:.2}°C, {}", city.trim(), ktoc(temperature), description);       
+        println!("Weather in {}: {:.2}°C, {}", city.trim(), temperature, description);       
     }
     else{
         println!("Error: {}",response.status());
